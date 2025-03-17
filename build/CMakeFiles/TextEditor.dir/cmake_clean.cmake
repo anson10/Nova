b1b2007f711737faf9bc7d6d487c48d705e37edf
anson10 <sansonmsa@gmail.com>
2025-03-17 23:35:22 +0530

    Major Updates

---
+++ build/CMakeFiles/TextEditor.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextEditor_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/TextEditor_autogen.dir/ParseCache.txt"
+  "TextEditor_autogen"
+  "CMakeFiles/TextEditor.dir/SyntaxHighlighter.cpp.o"
+  "CMakeFiles/TextEditor.dir/SyntaxHighlighter.cpp.o.d"
+  "CMakeFiles/TextEditor.dir/TextEditor.cpp.o"
+  "CMakeFiles/TextEditor.dir/TextEditor.cpp.o.d"
+  "CMakeFiles/TextEditor.dir/TextEditor_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/TextEditor.dir/TextEditor_autogen/mocs_compilation.cpp.o.d"
+  "CMakeFiles/TextEditor.dir/main.cpp.o"
+  "CMakeFiles/TextEditor.dir/main.cpp.o.d"
+  "TextEditor"
+  "TextEditor.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TextEditor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/TextEditor_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TextEditor_autogen"
+  "TextEditor_autogen/mocs_compilation.cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/TextEditor_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
